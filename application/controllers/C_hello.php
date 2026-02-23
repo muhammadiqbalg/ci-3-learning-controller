c6c2de8f6b12cb7ae3db66cd9be2cba9e9a488ed
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-02-23 12:34:49 +0700

    part 1 : controller

---
+++ application/controllers/C_hello.php
@@ -0,0 +1,21 @@
+<?php
+
+class C_hello extends CI_Controller {
+	function index() {
+		?>
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Hello World!</title>
+		</head>
+		<body>
+			<?php echo "Hello World"; ?>
+		</body>
+		</html>
+		<?php
+	}
+}
+
+?>
