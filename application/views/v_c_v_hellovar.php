7b6d6b113239e48142bc50a5b7d58634d6901680
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-02-23 13:34:47 +0700

    part 4 : controller, view, model, and styling with css

--- application/views/v_c_v_hellovar.php
+++ application/views/v_c_v_hellovar.php
@@ -4,8 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Controller View Variable</title>
+	<style type="text/css">
+		.container{
+			margin: 10px;
+			border: 1px solid #d0d0d0;
+			background: #333;
+			text: #eaeaea;
+			padding: 5px;
+			-webkit-box-shadow: 0 0 8px #d0d0d0;
+		}
+	</style>
 </head>
 <body>
-	<?php echo $halo; ?>
+	<div class="container">
+		<?php echo $halo; ?>
+	</div>
 </body>
 </html>
