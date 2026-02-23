85f9843492d1258fa1e86520478e3757841231c7
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-02-23 14:12:47 +0700

    css style back

--- application/views/v_c_v_hellovar.php
+++ application/views/v_c_v_hellovar.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Controller View Variable</title>
-	<!-- <style type="text/css">
+	<style type="text/css">
 		.container{
 			margin: 10px;
 			border: 1px solid #d0d0d0;
@@ -13,8 +13,7 @@
 			padding: 5px;
 			-webkit-box-shadow: 0 0 8px #d0d0d0;
 		}
-	</style> -->
-	<script src="https://cdn.tailwindcss.com"></script>
+	</style>
 </head>
 <body>
 	<div class="container">
